bc31ea97b46aaf274ed83be8bd3a8b948b869d7e
masterfiletable <masterfiletable@gmail.com>
2020-05-29 14:47:17 +0200

    added connection with lcm.js in index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const HTTP_SERVER_PORT = 3000;
 const express = require('express');
 const app = express();
 const addModule = require('./math-operations/add');
+const lcmModule = require('./math-operations/lcm');
 
 // setting routing https://expressjs.com/en/starter/basic-routing.html
 app.get('/api/add/:a/:b', (req, res) => {
@@ -11,6 +12,17 @@ app.get('/api/add/:a/:b', (req, res) => {
     try {
         const sum = { a: a, b: b, sum: addModule.add(a, b) };
         res.send(JSON.stringify(sum));
+    } catch (e) {
+        res.status(400).json({ error: e })
+    }
+})
+
+app.get('/api/lcm/:a/:b', (req, res) => {
+    const a = req.params.a;
+    const b = req.params.b;
+    try {
+        const lcm = { a: a, b: b, lcm: lcmModule.lcm(a, b) };
+        res.send(JSON.stringify(lcm));
     } catch (e) {
         res.status(400).json({ error: e })
     }
